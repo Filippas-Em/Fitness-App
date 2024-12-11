c57915947b62c913e37e052b742b20692f7eb308
Filippas-Em <emfiman02@gmail.com>
2024-12-11 13:19:26 +0200

    swipper complete

--- javaScript/swiper.js
+++ javaScript/swiper.js
@@ -1,7 +1,17 @@
-const swiper = new Swiper('.swiper', {
-    modules: [Swiper.Navigation, Swiper.Pagination],
-    loop: true,
-    slidesPerView: 1,
-    navigation: true,
-    pagination: { clickable: true },
+// Initialize Swiper after the DOM content is fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+        loop: false,
+        speed: 1200,
+        slidesPerView: 1,  // Show one slide at a time
+        navigation: {
+            nextEl: '.swiper-button-next',  // Correct selector for next button
+            prevEl: '.swiper-button-prev'   // Correct selector for prev button
+        },
+        pagination: {
+            el: '.swiper-pagination',       // Correct selector for pagination
+            clickable: true
+        }
+        
+    });
 });
