10575f2b758dc135bda4a8554fe1646f709393cf
Filippas-Em <emfiman02@gmail.com>
2024-12-11 11:44:09 +0200

    swiper module removed, added cdn

--- javaScript/swiper.js
+++ javaScript/swiper.js
@@ -1,15 +1,7 @@
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
 const swiper = new Swiper('.swiper', {
-    // configure Swiper to use modules
-    modules: [Navigation, Pagination],
+    modules: [Swiper.Navigation, Swiper.Pagination],
     loop: true,
     slidesPerView: 1,
     navigation: true,
     pagination: { clickable: true },
-  });
-  
+});
