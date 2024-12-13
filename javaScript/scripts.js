b80159264d0e483f764dab333932e5856cdef507
Filippas-Em <emfiman02@gmail.com>
2024-12-13 14:52:39 +0200

    Reservation options tab complete

--- javaScript/scripts.js
+++ javaScript/scripts.js
@@ -6,8 +6,7 @@ const menuLinks = offScreenMenu.querySelectorAll('a');
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
-
-    // Disable or enable scrolling
+    
     if (hamMenu.classList.contains("active")) {
         document.body.style.overflow = "hidden";
     } else {
@@ -20,5 +19,32 @@ menuLinks.forEach(link => {
   link.addEventListener('click', () => {
     hamMenu.classList.remove('active');
     offScreenMenu.classList.remove('active');
+  });
+});
+
+// reservation filter tab functionality
+
+$(document).ready(function () {
+  $(".dropdown-toggle").on("click", function () {
+      const $menu = $(this).next(".dropdown-menu"); 
+      const $arrow = $(this).find(".chevIcon");
+      $menu.slideToggle(); 
+      $arrow.toggleClass("rotate");
+  });
+});
+
+
+
+const filterToggles = document.querySelectorAll('.filterToggle');
+const filterTab = document.querySelector('.options');
+const mainContent = document.querySelector('.sqlData');
+const filterIcon = document.querySelector('#filterIcon');
+
+filterToggles.forEach((toggle) => {
+  toggle.addEventListener('click', () => {
+    console.log('clicked');
+    filterTab.classList.toggle('hidden');
+    mainContent.classList.toggle('move');
+    filterIcon.classList.toggle('none');
   });
 });
