76eb1967835bb78e7ebb1253e68e16bac15eabf8
Filippas-Em <emfiman02@gmail.com>
2025-01-03 16:19:13 +0200

    index news,discoutns fetching. Reservation css fix

---
+++ javaScript/formScripts.js
@@ -0,0 +1,41 @@
+const hamMenu = document.querySelector('.ham-menu');
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+
+
+
+//set date for today on the date picker instead of default empty value
+//put it in a new js file for the reservation page only
+
+// const today = new Date();
+// const formattedDate = today.toISOString().split("T")[0]; 
+// const datePicker = document.getElementById("datePicker");
+// datePicker.value = formattedDate;
+
+
+const mainFormInputs = document.querySelectorAll('.userInput');
+mainFormInputs.forEach((input => {
+    const label = input.previousElementSibling;
+
+    input.addEventListener('focus', () => {
+        label.classList.add('moveLabel');
+    }); 
+
+    input.addEventListener('blur', () => {
+        if (!input.value) {
+            label.classList.remove('moveLabel');
+        }
+    });
+})); 
+
+
+const closeButton = document.getElementById('closeForms');
+const openButton = document.querySelector('.signin');
+const form = document.querySelector('.loginFroms');
+const logIn = document.querySelector('.login');
+const signUp = document.querySelector('.signup');
+const loginBody = document.querySelector('.logInForm');
+const signUpBody = document.querySelector('.signupForm');
+
+
+
